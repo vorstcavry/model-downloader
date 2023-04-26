8f3a43b060154051d970a465a3dd6cc2342ac6d3
Vorst Cavry <vorstcavry@gmail.com>
2023-04-26 19:14:57 +0700

    Update model-downloader.py

--- scripts/model-downloader.py
+++ scripts/model-downloader.py
@@ -4,15 +4,15 @@ from modules import scripts, script_callbacks
 
 def folder(content_type):
     if content_type == "Checkpoint":
-       return gr.Textbox.update(value=" -d /content/cagliostro-colab-ui/models/Stable-diffusion")
+       return gr.Textbox.update(value=" -d /content/stable-diffusion-$masbro/models/Stable-diffusion")
     elif content_type == "Hypernetwork":
-         return gr.Textbox.update(value=" -d /content/cagliostro-colab-ui/models/hypernetworks")
+         return gr.Textbox.update(value=" -d /content/stable-diffusion-$masbro/models/hypernetworks")
     elif content_type == "TextualInversion/Embedding":
-         return gr.Textbox.update(value=" -d /content/cagliostro-colab-ui/embeddings")
+         return gr.Textbox.update(value=" -d /content/stable-diffusion-$masbro/embeddings")
     elif content_type == "VAE":
-         return gr.Textbox.update(value=" -d /content/cagliostro-colab-ui/models/VAE")
+         return gr.Textbox.update(value=" -d /content/stable-diffusion-$masbro/models/VAE")
     elif content_type == "Lora":
-         return gr.Textbox.update(value=" -d /content/cagliostro-colab-ui/models/Lora")
+         return gr.Textbox.update(value=" -d /content/stable-diffusion-$masbro/Lora")
 
 def cfn(filename1, filename, opt):
     if filename1 == "Use original Filename from the Source":
